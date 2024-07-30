ab75de1dcd831bcc84b915e1fef087d867da22f2
sagar4real <smagar816@gmail.com>
2024-07-30 05:51:02 +0545

    feat: footer update

--- application/views/layouts/admin/partials/footer.php
+++ application/views/layouts/admin/partials/footer.php
@@ -1,8 +1,10 @@
-<footer class="main-footer">
+<footer class="main-footer" style="display:flex; align-items:center; justify-content: space-between;">
+    <strong>Developed by Cloud Peak Private Limited </strong>
     <strong>
         Copyright &copy; <?php echo date('Y') ?>
         <a href="<?php echo $site_settings->web_url ?>""><?php echo $site_settings->site_name ?></a>.
-  </strong> All rights reserved.
+        All rights reserved.
+  </strong> 
   <!-- <div class=" pull-right d-none d-sm-inline-block">
             <b>Designed & Developed By :</b> <a href="https://nyatapol.com/" target="_blank">Nyatapol</a>
             </div> -->
